67c95b7c24dbbc6634c46312169d78947e880e2f
Lilianobi <obililian.n@gmail.com>
2025-06-05 16:26:15 +0100

    all

--- Favicon React/Favicon/vite.config.js
+++ Favicon React/Favicon/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/lilianobi.github.io/', 
+export default defineConfig({ 
   plugins: [react(), tailwindcss(),],
 })
